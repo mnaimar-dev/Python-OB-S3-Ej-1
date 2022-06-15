5c6284537c2ff0496a5a0019c1afdee925893b8e
Nicolas Aimar <Nicoaimar@Nicolass-MBP.fibertel.com.ar>
2022-06-15 03:40:20 -0300

    First Commit

---
+++ ejercicio-1.py
@@ -0,0 +1,23 @@
+Nombre = 'Nicolás'
+Apellidos = 'Aimar'
+Edad = 27
+Email = 'nicolas@correo.com'
+Telefono = 35166666
+Casado = False
+Con_Hijos = False
+Lista_de_Amigos = ['Matías', 'Mateo', 'Pedro']
+Peliculas_Vistas = {
+    'Pelicula 1': 'Harry Potter',
+    'Pelicula 2': 'Iron Man',
+    'Pelicula 3': 'The Big Short'
+}
+
+print('Nombre: ', Nombre)
+print('Apellido: ', Apellidos)
+print('Edad: ', Edad)
+print('Email: ', Email)
+print('Teléfono: ', Telefono)
+print('Casado: ', Casado)
+print('Con Hijos: ', Con_Hijos)
+print('Lista de Amigos: ', Lista_de_Amigos)
+print('Peliculas Vistas: ', Peliculas_Vistas)
